2687fb7dee55d989a456043f2fdf290132492bc1
VISHNUTHEJA <vishnutheja@VISHNUTHEJAs-MacBook-Pro.local>
2026-06-30 07:57:47 -0400

    day2 - reverse string

--- 02_Strings/01_Strings_Reverse/string-reverse.py
+++ 02_Strings/01_Strings_Reverse/string-reverse.py
@@ -1,1 +1,15 @@
 # String Reverse
+#method 1: Using slicing
+def reverse_string_slice(original_string):
+    return original_string[::-1]
+# method 2: Using a loop
+def reverse_string_iterator(text:str ) -> str:
+    return "".join(reversed(text))
+if __name__ == "__main__":
+    original_string = input("Enter a string to reverse: ")
+    reversed_slice = reverse_string_slice(original_string)
+    print(f"Original String: {original_string}")
+    print(f"Reversed String using slicing: {reversed_slice}")
+
+    reversed_iterator = reverse_string_iterator(original_string)
+    print(f"Reversed String using iterator: {reversed_iterator}")
